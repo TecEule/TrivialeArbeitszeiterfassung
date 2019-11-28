7a45544862212156fbe27cd1aef0968f6be4e649
TecEule <->
2019-11-28 14:51:54 +0100

    begin read/write values from file

---
+++ teCore/File.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teCore
+{
+  public class File
+  {
+
+    public File(string fileName)
+    {
+
+    }
+
+    public File(string filePath, string fileName)
+    {
+
+    }
+    
+
+    public object readValueFromFile()
+    {
+      object retValue = null;
+
+
+      return retValue;
+    }
+    
+
+  }
+}
--- teLogIn/teLogIn.cs
+++ teLogIn/teLogIn.cs
@@ -6,58 +6,68 @@ using System.Threading.Tasks;
 
 namespace teLogIn
 {
-    public class teLogIn
+  public class teLogIn
+  {
+
+    private static teLogIn _instance;
+    public static teLogIn Instance
+    {
+      get
+      {
+        if (_instance == null)
+          _instance = new teLogIn();
+
+        return _instance;
+      }
+    }
+
+    private teLogIn()
     {
 
-        private static teLogIn _instance;
-        public static teLogIn Instance
-        {
-            get
-            {
-                if (_instance == null)
-                    _instance = new teLogIn();
-
-                return _instance;
-            }
-        }
-
-        private teLogIn()
-        {
-
-        }
-        public bool checkLogIn(string benutzer, string passwort)
-        {
-            bool correct = false;
-
-            correct = compareData(benutzer, passwort);
-
-            return correct;
-        }
-
-        private bool compareData(string benutzer, string passwort)
-        {
-            bool correct = false;
-
-
-            string passwortFromFile = getPasswort(benutzer);
-
-            if(passwort.Equals(passwortFromFile))
-            {
-                correct = true;
-            }
-
-            return correct;
-        }
-
-        private string getPasswort(string benutzer)
-        {
-            string password = string.Empty;
-
-            // zum testen
-            password = "sesam";
-
-            return password;
-        }
-
     }
+
+    public bool checkLogIn(AccountModelView account)
+    {
+      return checkLogIn(account.Username, account.Password);
+    }
+    public bool checkLogIn(string benutzer, string passwort)
+    {
+      bool correct = false;
+
+      correct = compareData(benutzer, passwort);
+
+      return correct;
+    }
+
+    private bool compareData(string benutzer, string passwort)
+    {
+      bool correct = false;
+
+
+      string passwortFromFile = getPasswort(benutzer);
+
+      if (passwort.Equals(passwortFromFile))
+      {
+        correct = true;
+      }
+
+      return correct;
+    }
+
+    private string getPasswort(string benutzer)
+    {
+      string password = string.Empty;
+
+      password = readPasswordFromFile(benutzer);
+
+      return password;
+    }
+
+    private string readPasswordFromFile(string benutzer)
+    {
+      string password = string.Empty;
+
+      return password;
+    }
+  }
 }
